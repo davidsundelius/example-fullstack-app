d6f41833986329fade1ce9a73e379a6fef47bf68
David Sundelius <david.sundelius@gmail.com>
2020-10-05 19:56:59 +0200

    Added read and write to database as well as a service script for Ubuntu machines to run the application as a service

--- backend/src/main/java/com/backend/demo/DemoApplication.java
+++ backend/src/main/java/com/backend/demo/DemoApplication.java
@@ -1,26 +1,29 @@
 package com.backend.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@Autowired
+	private MessageRepository messageRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@PostMapping("/message")
+	public @ResponseBody String createNewMessage(@RequestParam String message) {
+		Message newMessage = new Message(message);
+		messageRepository.save(newMessage);
+		return "{\"success\": true}";
+	}
+
 	@GetMapping("/messages")
-	public MessageModel[] getMessages() {
-		MessageModel[] results = new MessageModel[5];
-		results[0] = new MessageModel("Message 1");
-		results[1] = new MessageModel("Message 2");
-		results[2] = new MessageModel("Message 3");
-		results[3] = new MessageModel("Message 4");
-		results[4] = new MessageModel("Message 6");
-		return results;
+	public @ResponseBody Iterable<Message> getMessages() {
+		return messageRepository.findAll();
 	}
 }
